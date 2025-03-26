99c1eb0378cc9696cda38bf72a400f73a9731e89
RomanShubets <roman.shubets@lnu.edu.ua>
2025-03-26 16:07:49 +0200

    Update money.h

--- money.h
+++ money.h
@@ -1,15 +1,14 @@
 #ifndef MONEY_H
 #define MONEY_H
 
-// Оголошення структури Money та функцій
 struct Money {
-    int grn;
-    int kop;
+    int grn;  
+    int kop; 
 };
 
-void mult(Money a, int col);
-void sum(Money a, Money b);
-void m_round(Money a);
+void mult(Money &a, int col);
+void sum(Money &a, Money &b);
+void m_round(Money &m);
 void printMoney(Money a);
 
-#endif // MONEY_H
+#endif 
